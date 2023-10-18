1cdd2cef9d7e3a6d6c400dbe9a2e4135e6401428
mohadeseh chopani <chopanimohadeseh0@gmail.com>
2023-10-18 20:58:22 +0330

    add and get data from cloud firestore

--- app/src/main/java/com/example/testcloudfirebase/MainActivity.java
+++ app/src/main/java/com/example/testcloudfirebase/MainActivity.java
@@ -7,13 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.testcloudfirebase.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -35,37 +38,71 @@ public class MainActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
         Map<String, Object> user = new HashMap<>();
-        user.put("firstName", "Ali");
-        user.put("lastName", "Farahani");
-        user.put("born", 2007);
 
-        db.collection("users").add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, "Error adding document:" + e.getMessage(), e);
-                    }
-                });
+        CollectionReference reference=db.collection("users");
+        DocumentReference documentReference=reference.document("iGb3o49fWRbrDf5L3Plu");
 
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        binding.btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + "=>" + document.getData());
-                            }
-                        } else {
-                            Log.e(TAG, "Error getting documents.", task.getException());
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                        if(documentSnapshot.exists()){
+                            String first_name,last_name,born;
+                            first_name=documentSnapshot.getString("firstName");
+                            last_name=documentSnapshot.getString("lastName");
+                            born=documentSnapshot.getString("born");
+
+                            binding.inName.setText(first_name);
+                            binding.inUsername.setText(last_name);
+                            binding.inBorn.setText(born);
                         }
                     }
                 });
+            }
+        });
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.put("firstName", binding.inName.getText().toString());
+                user.put("lastName", binding.inUsername.getText().toString());
+                user.put("born", binding.inBorn.getText().toString());
+
+                db.collection("users").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                binding.inName.setText(" ");
+                                binding.inUsername.setText(" ");
+                                binding.inBorn.setText(" ");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "Error adding document:" + e.getMessage(), e);
+                            }
+                        });
+
+//                db.collection("users")
+//                        .get()
+//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                                        Log.d(TAG, document.getId() + "=>" + document.getData());
+//                                    }
+//                                } else {
+//                                    Log.e(TAG, "Error getting documents.", task.getException());
+//                                }
+//                            }
+//                        });
+            }
+        });
     }
 }
